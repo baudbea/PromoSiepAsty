006e3df4ff9ca74ed866da58fe0e5e84f479c8f8
TheRex <83464936+Trex2007@users.noreply.github.com>
2025-03-08 18:12:35 +0100

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -59,3 +59,18 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+const toggleBtn = document.getElementById("themeToggle");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    toggleBtn.textContent = "☀️";
+}
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    const isDark = document.body.classList.contains("dark-mode");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+    toggleBtn.textContent = isDark ? "☀️" : "🌙";
+});
